31c012d6856bd7379cc226814d8d6e060987ac9f
Jon Cooke <git@darkpenguin.org>
2020-01-14 21:17:56 +0000

    Display the avatar of the user next to the message.

--- src/components/message.js
+++ src/components/message.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Message = ({ id, userId, message, timestamp }) => (
+const Message = ({ user, message, timestamp }) => (
   <div>
-    <div>{id}</div>
-    <div>{userId}</div>
-    <div>{message}</div>
+    <div>
+      <img src={user.avatar} alt={`${user.firstName} ${user.lastName}`} width={100} height={100} />
+      {message}
+    </div>
     <div>{new Date(timestamp).toLocaleString()}</div>
   </div>
 );
 
+const userType = {
+  id: PropTypes.string,
+  avatar: PropTypes.string,
+  email: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+};
+
 Message.propTypes = {
   id: PropTypes.string,
-  userId: PropTypes.string,
+  user: PropTypes.shape(userType),
   message: PropTypes.string,
   timestamp: PropTypes.string,
 };
--- src/components/message.test.js
+++ src/components/message.test.js
@@ -13,6 +13,14 @@ it('renders correctly', () => {
     userId: 'fe27b760-a915-475c-80bb-7cdf14cc6ef3',
     message: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.',
     timestamp: '2017-02-09T04:27:38Z',
+    user: {
+      id: 'fe27b760-a915-475c-80bb-7cdf14cc6ef3',
+      firstName: 'Albert',
+      lastName: 'Rose',
+      email: 'arosec@bbb.org',
+      avatar: 'http://dummyimage.com/100x100.jpg/5fa2dd/ffffff',
+      ip: '20.79.231.223',
+    },
   };
   const tree = shallow(<Message {...props} />);
   expect(tree).toMatchSnapshot();
